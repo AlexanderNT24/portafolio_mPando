b6e7b0b3568e67e0b2dac1cb2e2e137e1d5f73d4
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2023-02-16 16:31:56 -0500

    add views and urls biography, contact, about, blog and home

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -6,3 +6,9 @@ def home(request):
     projects=Project.objects.all()
     #Enviamos en un diccionario
     return render(request,'home.html',{'projects':projects})
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def biography(request):
+    return render(request,'biography.html')
