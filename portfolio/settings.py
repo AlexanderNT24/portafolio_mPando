ab6169391ce315aa9e0ac1b0067e9ffb1ff0b35d
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2023-04-05 01:00:38 -0500

    change host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$&(dj(e=-^$ey(y$33y3oturw&z&6i6+tt9vo%fjssdb^cp*@f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['176.126.87.42']
+ALLOWED_HOSTS = ['176.126.87.42', 'localhost']
 
 
 # Application definition
