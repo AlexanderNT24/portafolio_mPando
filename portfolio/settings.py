cccd82677d68bfced613c9e529844e70834a39f9
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2023-04-05 01:16:41 -0500

    change settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
+settings.FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
