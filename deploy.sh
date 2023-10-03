4f32c650ad79f10ae0a068e917e6b651b1ae8b7f
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2023-10-03 00:42:27 -0500

    add deploy bash

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Detener y eliminar el contenedor existente
+docker stop mpando_portfolio
+docker rm mpando_portfolio
+
+# Construir la imagen Docker
+docker build -t mpando_portfolio .
+
+# Ejecutar el contenedor
+docker run -p 9086:9086 --name mpando_portfolio mpando_portfolio
+
+# Reiniciar el contenedor
+docker restart mpando_portfolio
