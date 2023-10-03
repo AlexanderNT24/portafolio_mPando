b62e19ab9fc47aced9768990483f49da6e7bcc72
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2023-10-03 01:29:21 -0500

    add view image

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
-#Import model proyects from model
-from .models import Exhibition,Exhibition_View
-
+from django.utils.safestring import mark_safe  # Importa mark_safe
+from .models import Exhibition, Exhibition_View
 
 class ExhibitionViewInline(admin.TabularInline):
     model = Exhibition_View
+    fields = ('title', 'description', 'display_image')
+    extra = 0
+    readonly_fields = ('display_image',)
+
+    def display_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="100" height="100" />')
+        else:
+            return 'No image'
+
+    display_image.short_description = 'Image'
 
 class ExhibitionAdmin(admin.ModelAdmin):
-    inlines = [
-        ExhibitionViewInline,
-    ]
-    list_display = ('title',) 
+    inlines = [ExhibitionViewInline]
+    list_display = ('title',)
 
 admin.site.register(Exhibition, ExhibitionAdmin)
 admin.site.register(Exhibition_View)
