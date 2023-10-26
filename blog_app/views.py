f9b162f7ac38f98aac0c2c5321909fa476320061
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2023-10-26 11:55:50 -0500

    fix list blog

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 def render_posts(request):
-    posts=Post.objects.all()
-    return render(request, 'posts.html',{'posts':posts})
+    posts = Post.objects.order_by('-start_date')
+    return render(request, 'posts.html', {'posts': posts})
+
+
 def post_detail(request,post_id):
     #Funcion de django para obtener un valor o retornar 404
     post=get_object_or_404(Post,pk=post_id)
