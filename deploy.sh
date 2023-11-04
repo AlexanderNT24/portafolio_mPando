be1f8811c0bb1394e0e818cdaaf3996ac61d5354
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2023-11-04 06:13:10 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# Detener y eliminar el contenedor existente
-docker stop mpando_portfolio
-docker rm mpando_portfolio
 
-# Construir la imagen Docker
-docker build -t mpando_portfolio .
+# Pedir confirmación al usuario
+read -p "¿Has realizado una copia de seguridad de tus datos? (y/n) " confirmacion
 
-# Ejecutar el contenedor
-docker run -p 9086:9086 --name mpando_portfolio mpando_portfolio
+if [ "$confirmacion" == "y" ]; then
+  # Detener y eliminar el contenedor existente
+  docker stop mpando_portfolio
+  docker rm mpando_portfolio
+  
+  # Construir la imagen Docker
+  docker build -t mpando_portfolio .
 
-# Reiniciar el contenedor
-docker restart mpando_portfolio
+  # Ejecutar el contenedor con el volumen montado
+  docker run -p 9086:9086 --name mpando_portfolio -v portfolio_data:/app/db mpando_portfolio
+else
+  echo "Asegúrate de hacer una copia de seguridad antes de continuar."
+fi
